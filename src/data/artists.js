1460feb34779e7ef2881db3cdea83ccf6c4da10a
ZeshanRasul <zeshanrasul90@gmail.com>
2016-10-06 23:43:09 +0100

    Added data and img directory

---
+++ src/data/artists.js
@@ -0,0 +1,152 @@
+"use strict";
+
+const artists = [
+  {
+    'id': 'jay-z',
+    'name': 'Jay Z',
+    'country': 'USA',
+    'birth': '1969',
+    'image': 'jay-z.jpg',
+    'cover': 'jay-z-cover.jpg',
+    'link': 'https://en.wikipedia.org/wiki/Jay_Z',
+    'albums': [
+      {'name': 'Reasonable Doubt', 'year': '1996' },
+      {'name': 'In My Lifetime, Vol.1', 'year': '1997' },
+      {'name': 'Vol. 2... Hard Knock Life', 'year': '1998' },
+      {'name': 'Vol. 3...  Life and Times of S.Carter', 'year': '1999' },
+      {'name': 'The Dynasty: Roc La Familia', 'year': '2000' },
+      {'name': 'The Blueprint', 'year': '2001' },
+      {'name': 'The Best of Both Worlds (with R. Kelly)', 'year': '2002' },
+      {'name': 'The Blueprint 2: The Gift & The Curse', 'year': '2002' },
+      {'name': 'The Black Album', 'year': '2003' },
+      {'name': 'Unfinished Business (with R.Kelly)', 'year': '2004' },
+      {'name': 'Kingdom Come', 'year': '2006' },
+      {'name': 'American Gangster', 'year': '2007' },
+      {'name': 'The Blueprint 3', 'year': '2009' },
+      {'name': 'Watch the Throne (with Kanye West)', 'year': '2011' },
+      {'name': 'Magna Carta Holy Grail', 'year': '2013' }
+    ],
+  },
+  {
+    'id': 'tupac',
+    'name': 'Tupac Shakur',
+    'country': 'USA',
+    'birth': '1971',
+    'image': 'tupac.jpg',
+    'cover': 'tupac-cover.jpg',
+    'link': 'https://en.wikipedia.org/wiki/Tupac_Shakur',
+    'albums': [
+      {'name': 'Strictly 4 My N****Z', 'year': '1993' },
+      {'name': 'Me Against the World', 'year': '1995' },
+      {'name': 'All Eyez on Me', 'year': '1996' },
+      {'name': 'The Don Killuminati: The 7 Day Theory', 'year': '1996' },
+      {'name': 'R U Still Down? (Remember Me)', 'year': '1997' },
+      {'name': 'Greatest Hits', 'year': '1998' },
+      {'name': 'Still I Rise', 'year': '1999' },
+      {'name': 'Until the End of Time', 'year': '2001' },
+      {'name': 'Better Dayz', 'year': '2002' },
+      {'name': 'Tupac: Resurrection', 'year': '2003' },
+      {'name': 'Loyal to the Game', 'year': '2004' },
+      {'name': 'Live at the House of Blues', 'year': '2005' }
+    ],
+  },
+  {
+    'id': 'eminem',
+    'name': 'Eminem (Marshall Mathers)',
+    'country': 'USA',
+    'birth': '1972',
+    'image': 'eminem.jpg',
+    'cover': 'eminem-cover.jpg',
+    'link': 'https://en.wikipedia.org/wiki/Eminem',
+    'albums': [
+      {'name': 'The Slim Shady LP', 'year': '1999' },
+      {'name': 'The Marshall Mathers LP', 'year': '2000' },
+      {'name': 'The Eminem Show', 'year': '2002' },
+      {'name': 'Encore', 'year': '2004' },
+      {'name': 'Curtain Call: The Hits', 'year': '2005' },
+      {'name': 'Eminem Presents: The Re-Up', 'year': '2006' },
+      {'name': 'Relapse', 'year': '2009' },
+      {'name': 'Recovery', 'year': '2010' },
+      {'name': 'The Marshall Mathers LP 2', 'year': '2013' }
+    ],
+  },
+  {
+    'id': 'snoop-dogg',
+    'name': 'Snoop Dogg (Cordozar Calvin Broadus, Jr.)',
+    'country': 'USA',
+    'birth': '1971',
+    'image': 'snoop-dogg.jpg',
+    'cover': 'snoop-dogg-cover.jpg',
+    'link': 'https://en.wikipedia.org/wiki/Snoop_Dogg',
+    'albums': [
+      {'name': 'Doggystyle', 'year': '1993' },
+      {'name': 'Murder Was the Case', 'year': '1994' },
+      {'name': 'Tha Doggfather', 'year': '1996' },
+      {'name': 'Da Game Is to Be Sold, Not to Be Told', 'year': '1998' },
+      {'name': 'No Limit Top Dogg', 'year': '1999' },
+      {'name': 'Tha Last Meal', 'year': '2000' },
+      {'name': 'Paid tha Cost to Be da Boss', 'year': '2002' },
+      {'name': 'R&G (Rhythm & Gangsta): The Masterpiece', 'year': '2004' }
+    ],
+  },
+  {
+    'id': 'kanye-west',
+    'name':
+    'country':
+    'birth':
+    'image':
+    'cover':
+    'link':
+    'albums': [
+
+    ],
+  },
+  {
+    'id': 'beastie-boys',
+    'name':
+    'country':
+    'birth':
+    'image':
+    'cover':
+    'link':
+    'albums': [
+
+    ],
+  },
+  {
+    'id': 'll-cool-j',
+    'name':
+    'country':
+    'birth':
+    'image':
+    'cover':
+    'link':
+    'albums': [
+
+    ],
+  },
+  {
+    'id': 'nas',
+    'name':
+    'country':
+    'birth':
+    'image':
+    'cover':
+    'link':
+    'albums': [
+
+    ],
+  },
+  {
+    'id': 'outkast',
+    'name':
+    'country':
+    'birth':
+    'image':
+    'cover':
+    'link':
+    'albums': [
+
+    ],
+  },
+]
