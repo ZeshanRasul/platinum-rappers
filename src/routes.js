2c9bdabab1ebfdd95e44ad559b6986d78369a895
ZeshanRasul <zeshanrasul90@gmail.com>
2016-10-07 21:21:37 +0100

    Added routes module

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, IndexRoute} from 'react-router';
+import Layout from './components/Layout';
+import IndexPage from './components/IndexPage';
+import ArtistPage from './components/ArtistPage';
+import NotFoundPage from './compenets/NotFoundPage';
+
+const routes = (
+  <Route path="/" component={Layout}>
+    <IndexRoute component={IndexPage}/>
+    <Route path="artist/:id" component={ArtistPage}/>
+    <Route path="*" component={NotFoundPage}>
+  </Route>
+)
+
+export default routes;
